690a28626c8e71895226c9e8374b85dab8a4405e
Rithwik Surya <surya.rithwik2005@gmail.com>
2025-09-03 23:34:46 +0530

    feat(friends):created groupchat

--- convex/conversations.ts
+++ convex/conversations.ts
@@ -1,4 +1,4 @@
-import { query } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 import { getUserByClerkId } from "./utils";
 import { v } from "convex/values";
 
@@ -52,12 +52,33 @@ export const get = query({
           ...otherMemberDetails,
           lastSeenMessageId: otherMembers[0].lastSeenMessage,
         },
+        otherMembers: [],
+        members: [],
       };
+    }else{
+      const otherMembers=(await Promise.all(allConversationMembers.filter(member=>member.memberId!==currentUser._id))).map(async membership=>{
+        const member=await ctx.db.get(membership.memberId)
+        if(!member) throw new Error("Member could not be found")
+        return {
+          username:member.username
+        }
+      })
     }
-
     // group conversation → return with members
+    const otherMembers = await Promise.all(
+      allConversationMembers
+        .filter((m) => m.memberId !== currentUser._id)
+        .map(async (membership) => {
+          const member = await ctx.db.get(membership.memberId);
+          if (!member) throw new Error("Member could not be found");
+          return { username: member.username };
+        })
+    );
+
     return {
       conversation,
+      otherMember: null,
+      otherMembers,
       members: await Promise.all(
         allConversationMembers.map(async (m) => ({
           ...m,
@@ -67,3 +88,35 @@ export const get = query({
     };
   },
 });
+
+export const createGroup=mutation({
+  args:{
+    members:v.array(v.id("users")),
+    name:v.string()
+  },
+  handler:async (ctx,args)=>{
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) throw new Error("Unauthorized");
+
+    const currentUser = await getUserByClerkId({
+      ctx,
+      clerkId: identity.subject,
+    });
+    if (!currentUser) throw new Error("User not found");
+
+    const conversationId=await ctx.db.insert("conversations",{
+      isGroup:true,
+      name:args.name
+    })
+
+    await Promise.all(
+      [...args.members,currentUser._id].map(async (memberId)=>{
+        await ctx.db.insert("conversationMembers",{
+          memberId,
+          conversationId
+        })
+      })
+    )
+
+  }
+})
